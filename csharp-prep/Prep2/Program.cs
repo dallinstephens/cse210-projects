22a29ebe840a2508ce0e92dcea5052a43f432d20
Dallin <dallins@juno.com>
2023-04-20 08:05:50 -0600

    Core Requirement 1 Complete at https://byui-cse.github.io/cse210-course-2023/unit01/csharp-2.html

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage: ");
+        string gradePercentageString = Console.ReadLine();
+
+        int gradePercentageInteger = int.Parse(gradePercentageString);
+        
+        if (gradePercentageInteger >= 90)
+        {
+            Console.Write("Letter Grade: A");
+        }
+        else if (gradePercentageInteger >= 80)
+        {
+            Console.Write("Letter Grade: B");
+        }
+        else if (gradePercentageInteger >= 70)
+        {
+            Console.Write("Letter Grade: C");
+        }
+        else if (gradePercentageInteger >= 60)
+        {
+            Console.Write("Letter Grade: D");
+        }
+        else if (gradePercentageInteger >= 0 && gradePercentageInteger < 60)
+        {
+            Console.Write("Letter Grade: F");
+        }
+        else 
+        {
+            Console.Write("The percentage you entered is invalid. Please try again.");
+        }                               
     }
 }
