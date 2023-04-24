2da6541d14986dbf38866be35bdb1d73f8bb56af
Dallin <dallins@juno.com>
2023-04-24 09:15:43 -0600

    W02 C# Prep 3: Core Requirement 1 https://byui-cse.github.io/cse210-course-2023/unit01/csharp-3.html

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Random randomGenerator = new Random();
+        // int number = randomGenerator.Next(1,11);
+        Console.Write("What is the magic number? ");
+        string answer1 = Console.ReadLine();
+        int magicNumber = int.Parse(answer1);
+
+        Console.Write("What is your guess? ");
+        string answer2 = Console.ReadLine();
+        int guess = int.Parse(answer2);
+
+        if (guess < magicNumber)
+        {
+            Console.Write("Higher");
+        }
+        else if (guess > magicNumber)
+        {
+            Console.Write("Lower");
+        }
+        else
+        {
+            Console.Write("You guessed it!");
+        }
     }
 }
+
+// Reference: https://byui-cse.github.io/cse210-course-2023/unit01/csharp-3.html
