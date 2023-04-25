027e2bff40906b66bc1f29eff4b960e79cf8c8bc
Dallin <dallins@juno.com>
2023-04-24 19:06:05 -0600

    W02 C# Prep 3: Stretch Challenge 2 https://byui-cse.github.io/cse210-course-2023/unit01/csharp-3.html

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,40 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Reference: https://www.w3schools.blog/c-random-number
-        // These 2 lines below creates a number between 1 and 100. 
-        // It can create the number "1" or "100", but not "101".
-        Random randomGenerator = new Random();
-        int magicNumber = randomGenerator.Next(1,101);
+        // The variable "response" is assigned "yes" to initially enter the loop.
+        string response = "yes";
 
-        int guess;
-
-        // The variable "n" is the number of guesses the user has made.
-        int n = 0;    
-
-        do 
+        while (response == "yes")
         {
-            Console.Write("What is your guess? ");
-            string answer = Console.ReadLine();
-            guess = int.Parse(answer);
+            // Reference: https://www.w3schools.blog/c-random-number
+            // These 2 lines below creates a number between 1 and 100. 
+            // It can create the number "1" or "100", but not "101".
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1,101);
 
-            if (guess < magicNumber)
-            {
-                Console.WriteLine("Higher");
-            }
-            else if (guess > magicNumber)
-            {
-                Console.WriteLine("Lower");
-            }
-            else
-            {
-                Console.WriteLine("You guessed it!");
-            }
+            int guess;
+
             // The variable "n" is the number of guesses the user has made.
-            // n += 1 is the same as n =  n + 1.
-            n += 1;
-        } while (guess != magicNumber);
-        Console.Write($"It took you {n} guess(es).");
+            int n = 0;    
+
+            do 
+            {
+                Console.Write("What is your guess? ");
+                string answer = Console.ReadLine();
+                guess = int.Parse(answer);
+
+                if (guess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+                // The variable "n" is the number of guesses the user has made.
+                // n += 1 is the same as n =  n + 1.
+                n += 1;
+            } while (guess != magicNumber);
+            Console.WriteLine($"It took you {n} guess(es).\n");
+
+            Console.Write("Do you want to play again (yes or no)? ");
+            response = Console.ReadLine();
+        }
     }
 }
 
