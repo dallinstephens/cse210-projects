55ad9926d552073b537ae08229f0a49ceeb7430a
Dallin <dallins@juno.com>
2023-05-01 19:52:46 -0600

    W02 Prove Step 3: Create the Job class https://byui-cse.github.io/cse210-course-2023/unit02/prepare.html

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,22 @@
+public class Job
+{
+    // The C# convention is to start member variables with an underscore _
+    public string _company = "";
+    public string _jobTitle = "";
+    public int _startYear;
+    public int _endYear;
+
+    // A special method, called a constructor that is invoked using the
+    // new keyword followed by the class name and parentheses.
+    public Job()
+    {
+    }
+
+    // A method that displays the 
+    public void DisplayJobDetails()
+    {
+        Console.WriteLine($"{_jobTitle} ({_company}) {_startYear}-{_endYear}");
+    }
+}
+
+// Reference: https://byui-cse.github.io/cse210-course-2023/unit02/prepare.html
